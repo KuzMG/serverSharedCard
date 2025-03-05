a76f1af532cb06b0852c003ba880d073025cd1cd
Mikhail <mifa_224@mail.ru>
2025-03-05 16:53:56 +0300

    все работает

--- src/main/java/com/project/sharedCardServer/restController/Authentication.java
+++ src/main/java/com/project/sharedCardServer/restController/Authentication.java
@@ -26,9 +26,6 @@ import com.project.sharedCardServer.restController.dto.AccountResponse;
 import com.project.sharedCardServer.restController.dto.AuthResponse;
 import com.project.sharedCardServer.restController.dto.DictionaryResponse;
 import com.project.sharedCardServer.restController.dto.RegistrationBody;
-import io.micrometer.core.annotation.Timed;
-import io.micrometer.core.instrument.MeterRegistry;
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -38,7 +35,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
-import java.util.concurrent.atomic.DoubleAccumulator;
 
 @RestController
 public class Authentication {
@@ -73,33 +69,18 @@ public class Authentication {
     public static final String HEADER_ID_USER = "id-user";
     public static final String HEADER_PASSWORD_USER = "password-user";
 
-    private DoubleAccumulator metricAuthentication;
-    private DoubleAccumulator metricRegistration;
-    private DoubleAccumulator metricVerification;
-    @Autowired
-    private MeterRegistry metricRegistry;
-    @PostConstruct
-    private void init(){
-        metricAuthentication = new DoubleAccumulator((x,y) -> y,0.0d);
-        metricRegistration = new DoubleAccumulator((x,y) -> y,0.0d);
-        metricVerification = new DoubleAccumulator((x,y) -> y,0.0d);
-        metricRegistry.gauge("metric_authentication",metricAuthentication);
-        metricRegistry.gauge("metric_registration",metricRegistration);
-        metricRegistry.gauge("metric_verification",metricVerification);
-    }
-    @Timed(value ="auth.timed.gg")
+
     @GetMapping("/authentication")
     public ResponseEntity<AuthResponse> authentication(@RequestParam("login") String login, @RequestParam("password") String password) {
         if (userDao.authentication(login, password)) {
             UUID userId = userDao.getUserAccount(login).getId();
             UUID groupId = groupDao.getDefaultGroup(login);
-            metricAuthentication.accumulate(metricAuthentication.get()+1);
             return new ResponseEntity<>(new AuthResponse(userId, groupId), HttpStatus.OK);
         }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
-    @Timed("registr")
+
     @PostMapping("/registration")
     public ResponseEntity<String> registration(@RequestBody RegistrationBody body) {
         String email = body.getEmail();
@@ -110,7 +91,7 @@ public class Authentication {
         Double weight = body.getWeight();
         Integer height = body.getHeight();
         if (!userDao.isExist(email)) {
-            UUID userId = userDao.createUser(email, password,name,date,gender,height,weight);
+            UUID userId = userDao.createUser(email, password, name, date, gender, height, weight);
             String userPic = fileManager.saveDefaultUserPic(userId);
             userDao.updatePic(userId, userPic);
 
@@ -138,7 +119,6 @@ public class Authentication {
                 Thread thread = new Thread(() -> CodeSender.send(email, code));
                 thread.start();
                 userDao.saveCode(email, code, dateNow, ++countCode);
-                metricRegistration.accumulate(metricRegistration.get() +1);
                 return ResponseEntity.ok().build();
             } else {
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Превышено количетсво отправленных сообщений!");
@@ -148,7 +128,6 @@ public class Authentication {
             Thread thread = new Thread(() -> CodeSender.send(email, code));
             thread.start();
             userDao.saveCode(email, code, dateNow, 1);
-            metricRegistration.accumulate(metricRegistration.get() +1);
             return ResponseEntity.ok().build();
         }
     }
@@ -159,7 +138,6 @@ public class Authentication {
             userDao.verification(login);
             UUID idUser = userDao.getUserAccount(login).getId();
             UUID idGroup = groupDao.getDefaultGroup(login);
-            metricVerification.accumulate(metricVerification.get() +1);
             return new ResponseEntity<>(new AuthResponse(idUser, idGroup), HttpStatus.OK);
         } else {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
@@ -192,8 +170,8 @@ public class Authentication {
         }
         AccountResponse accountResponse = new AccountResponse(
                 userDao.getUsers(idUser),
-                groupDao.getAll(idUser,true),
-                groupUsersDao.getAll(idUser,true),
+                groupDao.getAll(idUser, true),
+                groupUsersDao.getAll(idUser, true),
                 checkDao.getAll(idUser),
                 targetDao.getAll(idUser)
         );
